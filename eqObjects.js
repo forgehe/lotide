22ed4690ecf4fc046d82c2c882cf772994411b1c
Richard Liao <liao098@gmail.com>
2020-02-05 22:09:31 +0000

    finishing eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -7,6 +7,19 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
@@ -30,17 +43,20 @@ const eqObjects = function(object1, object2) {
 };
 
 const compareKeyValues = function(argsObj1, argsObj2) {
-  if (argsObj1 === argsObj2) {
-    return true;
+  if (Array.isArray(argsObj1) === true || Array.isArray(argsObj2) === true) {
+    return eqArrays(argsObj1, argsObj2);
   } else {
-    return false;
+    if (argsObj1 === argsObj2) {
+      return true;
+    } else {
+      return false;
+    }
   }
 };
 
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
- // => true
 
 assertEqual(eqObjects(ab, ba), true);
 
@@ -54,3 +70,10 @@ assertEqual(eqObjects(ab, abb), false); // => false
 const abd = { a: "1", b: "2", d: "3" };
 assertEqual(eqObjects(abd, abc), false); // => false
 
+console.log(`Array tests now:`);
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
