c5f5eba490cd60277dd89863d027b637c9dcb139
Richard Liao <liao098@gmail.com>
2020-02-08 00:48:03 +0000

    implemented recursion in eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -9,25 +9,42 @@ const assertEqual = function(actual, expected) {
 
 // Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 const eqArrays = function(array1, array2) {
+  let bool = true;
   if (array1.length !== array2.length) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+    if (Array.isArray(array1[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        bool = false;
+      }
+    } else {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
     }
+    
   }
-  return true;
+  return bool;
 };
 
 // TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 
 
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays(["1", "2",], ["1", "2", 3]), false);
+// assertEqual(eqArrays([], []), true);
+// assertEqual(eqArrays(["1", "2",], ["1", "2", 3]), false);
+
+// testing for multiple nested arrays
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+
+assertEqual(eqArrays([1, [2, [3]], 4, 5, [[[6, [[7]], [[8, 9]]]]], 10], [1, [2, [3]], 4, 5, [[[6, [[7]], [[8, 9]]]]], 10]), true);
+
--- eqObjects.js
+++ eqObjects.js
@@ -74,28 +74,28 @@ const compareKeyValues = function(argsObj1, argsObj2) {
 };
 
 
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
 
-// assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects(ab, ba), true);
 
-// const abc = { a: "1", b: "2", c: "3" };
-// const abb = { a: "1", b: "3" };
-// const aab = { a: "2", b: "2" };
-// assertEqual(eqObjects(ab, abc), false); // => false
-// assertEqual(eqObjects(ab, aab), false); // => false
-// assertEqual(eqObjects(ab, abb), false); // => false
+const abc = { a: "1", b: "2", c: "3" };
+const abb = { a: "1", b: "3" };
+const aab = { a: "2", b: "2" };
+assertEqual(eqObjects(ab, abc), false); // => false
+assertEqual(eqObjects(ab, aab), false); // => false
+assertEqual(eqObjects(ab, abb), false); // => false
 
-// const abd = { a: "1", b: "2", d: "3" };
-// assertEqual(eqObjects(abd, abc), false); // => false
+const abd = { a: "1", b: "2", d: "3" };
+assertEqual(eqObjects(abd, abc), false); // => false
 
-// console.log(`Array tests now:`);
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// assertEqual(eqObjects(cd, dc), true); // => true
+console.log(`Array tests now:`);
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
 
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertEqual(eqObjects(cd, cd2), false); // => false
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
 
 assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
 assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
