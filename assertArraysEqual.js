34a1c3384e4bcd9dcfbc493b099d51f4d5ae143f
Richard Liao <liao098@gmail.com>
2020-02-11 02:52:55 +0000

    refactor and cleaning up code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,7 @@
 const eqArrays = require("./eqArrays");
 
-const assertArraysEqual = function(array1, array2) {
+const assertArraysEqual = (array1, array2) => {
   console.log(`The arrays: [${array1}] and [${array2}] are ${eqArrays(array1, array2) ? "" : "not "}equal.`);
 };
-
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-// assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-
-// assertArraysEqual(eqArrays([], []));
-// assertArraysEqual(eqArrays(["1", "2",], ["1", "2", 3]));
 
 module.exports = assertArraysEqual;
--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,58 +1,12 @@
-// const assertEqual = require("./assertEqual");
-
-// const eqArrays = require("./eqArrays");
-
 const eqObjects = require("./eqObjects");
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 
-// In this simple scenario, two objects are equal when:
-// They have the same number of keys
-// The value for each key in one object is the same as the value for that same key in the other object
-
-
-const assertObjectsEqual = function(object1, object2) {
+const assertObjectsEqual = (object1, object2) => {
   const inspect = require('util').inspect; // <= add this line
-  // if (eqObjects(object1, object2) === true) {
-  //   console.log(`😀 Assert assertObjectsEqual Equal: \n${JSON.stringify(object1)} \n===  \n${JSON.stringify(object2)}`);
-  // } else {
-  //   console.log(`😡 Assert assertObjectsEqual Not Equal: \n${JSON.stringify(object1)} \n!== \n${JSON.stringify(object2)}`);
-  // }
   if (eqObjects(object1, object2) === true) {
     console.log(`😀 Assert assertObjectsEqual Equal: \n${inspect(object1)} \n===  \n${inspect(object2)}`);
   } else {
     console.log(`😡 Assert assertObjectsEqual Not Equal: \n${inspect(object1)} \n!== \n${inspect(object2)}`);
   }
 };
-
-
-
-// const testA = {
-//   testcase1: "one",
-//   testcase2: "two"
-// }
-
-// const testB = {
-//   testcase2: "two",
-//   testcase1: "one"
-// }
-
-// const testC = {
-//   testcase2: "two",
-//   testcase1: ["one", 2, 3, "eight"]
-// }
-
-// const testD = {
-//   testcase1: "one",
-//   testcase2: "two"
-// }
-
-// const testE = {
-//   testcase1: ["one", 2, 3, "eight"],
-//   testcase2: "two"
-// }
-// assertObjectsEqual(testA, testB);
-// assertObjectsEqual(testC, testD);
-// assertObjectsEqual(testC, testE);
 
 module.exports = assertObjectsEqual;
